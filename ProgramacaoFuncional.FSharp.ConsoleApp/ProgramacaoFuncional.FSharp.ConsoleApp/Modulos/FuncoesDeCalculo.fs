fc248f8d7b3cbe38ade27d4ef40a3b2a51e0f293
Andrey Frazatti Alves <desenvolvedorcsharp@outlook.com>
2017-10-09 20:38:07 -0300

    Apenas brincando com o F#. Explorando alguns conceitos já aprendidos.

---
+++ ProgramacaoFuncional.FSharp.ConsoleApp/ProgramacaoFuncional.FSharp.ConsoleApp/Modulos/FuncoesDeCalculo.fs
@@ -0,0 +1,9 @@
+﻿module FuncoesDeCalculo
+
+let somarDoisNumeros n1 n2 = n1 + n2
+
+let subtrairDoisNumeros n1 n2 = n1 - n2
+
+let multiplicarDoisNumeros n1 n2 = n1 * n2
+
+let dividirDoisNumeros n1 n2 = n1 / n2
---
+++ ProgramacaoFuncional.FSharp.ConsoleApp/ProgramacaoFuncional.FSharp.ConsoleApp/Program.fs
@@ -0,0 +1,27 @@
+﻿open System
+open FuncoesDeCalculo
+
+let executarMultiplasOperacoes n1 n2 operacoesMatematicas =    
+    for operacao in operacoesMatematicas do
+        printfn "%i" (operacao n1 n2)
+
+[<EntryPoint>]
+let main argv = 
+    
+    printfn "Informe o 1º número:"
+    let n1 = Convert.ToInt32(Console.ReadLine())
+
+    printfn "Informe o 2º número:"
+    let n2 = Convert.ToInt32(Console.ReadLine())
+
+    let operacoesMatematicas =
+        [|
+             somarDoisNumeros
+             subtrairDoisNumeros
+             multiplicarDoisNumeros
+             dividirDoisNumeros
+        |]
+
+    executarMultiplasOperacoes n1 n2 operacoesMatematicas
+
+    0
