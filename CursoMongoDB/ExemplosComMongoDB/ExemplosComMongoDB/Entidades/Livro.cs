f78d06f42df73b3f628be0a7c79766127eded92d
Andrey Frazatti Alves <desenvolvedorCSharp@outlook.com>
2017-11-28 18:44:26 -0200

    MongoDB e Asp.NET Core 2

---
+++ CursoMongoDB/ExemplosComMongoDB/ExemplosComMongoDB/Entidades/Livro.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Collections.Generic;
+
+namespace ExemplosComMongoDB.AspNETCore.Entidades
+{
+    public class Livro
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+        public int Ano { get; set; }
+        public int Paginas { get; set; }
+        public List<string> Assunto { get; set; }
+    }
+}
---
+++ CursoMongoDB/ExemplosComMongoDB/ExemplosComMongoDB/ProgramAsync.cs
@@ -0,0 +1,142 @@
+﻿using ExemplosComMongoDB.AspNETCore.Entidades;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ExemplosComMongoDB
+{
+    public class ProgramAsync
+    {
+        public static void Main(string[] args)
+        {
+            Task t = ManipulandoClasse();
+            Console.WriteLine("Pressione ENTER");
+            Console.ReadLine();
+        }
+
+        private static void MainSync()
+        {
+            Console.WriteLine("Esperando 10 segundos...");
+            Thread.Sleep(new TimeSpan(0, 0, 10));
+            Console.WriteLine("Pronto! Já esperei.");
+        }
+
+        private static async Task MainAsync()
+        {
+            Console.WriteLine("Esperando 10 segundos...");
+            await Task.Delay(new TimeSpan(0, 0, 10));
+            Console.WriteLine("Pronto! Já esperei.");
+        }
+
+        private static async Task CriandoBson()
+        {
+            BsonDocument livro = new BsonDocument
+            {
+                { "Titulo", "Guerra dos Tronos" }
+            };
+
+            livro.Add("Autor", "George R R Martin");
+            livro.Add("Ano", 1999);
+            livro.Add("Páginas", 856);
+
+            BsonArray assunto = new BsonArray();
+            assunto.Add("Fantasia");
+            assunto.Add("Ação");
+
+            livro.Add("Assunto", assunto);
+
+            Console.WriteLine(livro);
+        }
+
+        private static async Task AcessandoOMongoDB()
+        {
+            BsonDocument livro = new BsonDocument
+            {
+                { "Titulo", "Guerra dos Tronos" }
+            };
+
+            livro.Add("Autor", "George R R Martin");
+            livro.Add("Ano", 1999);
+            livro.Add("Páginas", 856);
+
+            BsonArray assunto = new BsonArray();
+            assunto.Add("Fantasia");
+            assunto.Add("Ação");
+
+            livro.Add("Assunto", assunto);
+
+            /* Acessando o servidor do MongoDB */
+            string connString = "mongodb://localhost:27017";
+
+            try
+            {
+                IMongoClient mongoClient = new MongoClient(connString);
+
+                /* Acessando o banco de dados do MongoDB
+                 * Observação:
+                 * Caso o banco de dados solicitado não exista, o GetDatabase criará. 
+                 * */
+                IMongoDatabase mongoDatabase = mongoClient.GetDatabase("biblioteca");
+
+                /* Acessando a coleção 
+                 * Observação:
+                 * Caso não exista a coleção solicitada, o GetCollection criará.
+                 * */
+                IMongoCollection<BsonDocument> livros = mongoDatabase.GetCollection<BsonDocument>("livros");
+
+                /* Inserindo um registro na coleção. */
+                await livros.InsertOneAsync(livro);
+
+                Console.WriteLine("Registro inserido com sucesso!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        private static async Task ManipulandoClasse()
+        {
+            Livro livro = new Livro
+            {
+                Titulo = "Guerra dos Tronos",
+                Autor = "George R R Martin",
+                Ano = 1999,
+                Paginas = 856,
+                Assunto = new List<string> { "Fantasia", "Ação" }
+            };
+
+            /* Acessando o servidor do MongoDB */
+            string connString = "mongodb://localhost:27017";
+
+            try
+            {
+                IMongoClient mongoClient = new MongoClient(connString);
+
+                /* Acessando o banco de dados do MongoDB
+                 * Observação:
+                 * Caso o banco de dados solicitado não exista, o GetDatabase criará. 
+                 * */
+                IMongoDatabase mongoDatabase = mongoClient.GetDatabase("biblioteca");
+
+                /* Acessando a coleção 
+                 * Observação:
+                 * Caso não exista a coleção solicitada, o GetCollection criará.
+                 * */
+                IMongoCollection<Livro> livros = mongoDatabase.GetCollection<Livro> ("livros");
+
+                /* Inserindo um registro na coleção. */
+                await livros.InsertOneAsync(livro);
+
+                Console.WriteLine("Registro inserido com sucesso!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
